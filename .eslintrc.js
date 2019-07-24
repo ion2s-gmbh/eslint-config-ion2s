6ab2b0062893acb6fdff9bcbe433717db58c60bb
Thomas Stern <thomas.stern@ion2s.com>
2019-07-24 15:16:48 +0200

    Fix rule indent for switch cases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 // https://eslint.org/docs/user-guide/configuring
+// https://standardjs.com/rules.html
 module.exports = {
     extends: [
         'standard',
     ],
     rules: {
         'comma-dangle': ['error', 'always-multiline'],
-        'indent': ['error', 4],
+        'indent': ['error', 4, { "SwitchCase": 1 }],
         'no-multi-spaces': ['error', { ignoreEOLComments: true }],
         'semi': ['error', 'always'],
     },
