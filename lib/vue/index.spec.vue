638a2e0e79ebe487755cc002146f4dec5e89e1e5
Orkan Özyurt <orkan.oezyurt@ion2s.com>
2019-09-23 12:26:09 +0200

    Add vue lint config, use by extending "ion2s/vue" in eslintrc

---
+++ lib/vue/index.spec.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <input v-model="name" type="text" @mouseover="doThings()">
+        <input
+            v-model="age"
+            class="styled-input"
+            type="text"
+            @mouseover="doThings()"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+    props: {
+        greet: {
+            type: String,
+            default: 'Test',
+        },
+    },
+    data () {
+        return {
+            name: '',
+            age: 0,
+        };
+    },
+    methods: {
+        doThings () {
+            console.log(`Hello ${this.name}`);
+            return this.age;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.styled-input {
+    background-color: red;
+}
+</style>
---
+++ vue.js
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: [
+        './lib/vue/index.js',
+    ],
+};
